f0bf2b468472240349fc467a3bf892d05553c2b6
etbteich <79092919+etbteich@users.noreply.github.com>
2021-08-21 12:26:40 +0300

    adding files

---
+++ app.js
@@ -0,0 +1,99 @@
+new Vue({
+el:'#app',
+data:{
+    playerHealth: 100,
+    monsterHealth: 100,
+    gameIsRunning:false,
+    turns:[]
+
+},
+methods:{
+    startGame: function(){
+        this.gameIsRunning=true;
+        this.playerHealth=100;
+        this.monsterHealth=100;
+        this.turns=[];
+    },
+    attack: function(){
+        var damage= this.calculatDamage(3,10);
+        this.monsterHealth-=damage;
+        this.turns.unshift({
+            isPlayer:true,
+            text:'Player hits Monster for ' +  damage
+        });
+        if(this.checkWin()){
+            return;
+        }
+
+
+
+        this.playerHealth-=this.calculatDamage(5,12);
+        this.monsterAttacks();
+
+    },
+    specialAttack:function(){
+        var damage= this.calculatDamage(10,20);
+        this.monsterHealth-= damage;
+        this.turns.unshift({
+            isPlayer:true,
+            text:'Player hits Monster hard for ' +  damage
+        });
+        if(this.checkWin()){
+            return;
+        }
+
+       this.monsterAttacks();
+
+    },
+    heal:function(){
+        if(this.playerHealth<=90){
+            this.playerHealth+=10;
+        }else{
+            this.playerHealth=100;
+        }
+        this.turns.unshift({
+            isPlayer:true,
+            text:'Player heals for 10'
+        });
+
+        this.monsterAttacks();
+    },
+    giveUp:function(){
+        this.gameIsRunning=false;
+    },
+    calculatDamage:function(min, max){
+        return Math.max(Math.floor(Math.random()*max)+1,min);
+    },
+    checkWin:function(){
+        if(this.monsterHealth<=0){
+            if(confirm('You Won! New Game?')){
+                this.startGame();
+            }
+            else{
+                this.gameIsRunning=false;
+            }
+
+            return true;
+        }
+        else if(this.playerHealth<=0){
+            if(confirm('You Lost! New Game?')){
+                this.startGame();
+            }
+            else{
+                this.gameIsRunning=false;
+            }
+            return true;
+        }
+        return false;
+    },
+    monsterAttacks:function(){
+        var damage=this.calculatDamage(5,12);
+        this.playerHealth-=damage;
+        this.turns.unshift({
+            isPlayer:false,
+            text:'Monster hits Player for '+ damage
+        });
+        this.checkWin();
+    }
+}
+});
